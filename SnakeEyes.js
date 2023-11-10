ebb1c297483b5eb9748fe6bcfa41a99089dae879
Samuel Valencia <68522351+svalencia014@users.noreply.github.com>
2023-11-10 07:38:35 -0500

    Update SnakeEyes.js

--- SnakeEyes.js
+++ SnakeEyes.js
@@ -1,11 +1,15 @@
-function start(){
+function main() {
+	let attempts = 0;
     while(true){
-        var roll1 = Randomizer.nextInt(1, 6);
-        var roll2 = Randomizer.nextInt(1, 6);
-        println(roll1 + ", " + roll2);
+        let roll1 = Randomizer.nextInt(1, 6);
+        let roll2 = Randomizer.nextInt(1, 6);
+        console.log(`Rolled: ${roll1} ${roll2}`);
+		attempts++;
         if(roll1 == 1 && roll2 == 1){
             break; 
         }
     }
-	
+	console.log(`It took you ${attempts} rolls to get snake eyes.`);
 }
+
+main();
