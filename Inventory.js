292c7aac7d71195c8dc844b4199d1d655f9855d9
Samuel Valencia <68522351+svalencia014@users.noreply.github.com>
2023-11-09 22:09:27 -0500

    Update Inventory.js

--- Inventory.js
+++ Inventory.js
@@ -1,26 +1,28 @@
-var STARTING_ITEMS_IN_INVENTORY = 20;
+let STARTING_ITEMS_IN_INVENTORY = 20;
 
-function start(){
-    
+function main(){
     while(STARTING_ITEMS_IN_INVENTORY >= 1){
-        println("We have " + STARTING_ITEMS_IN_INVENTORY + " items in inventory.");
-        var buy = readInt("How many would you like to buy? ");
+        console.log("We have " + STARTING_ITEMS_IN_INVENTORY + " items in inventory.");
+        let buy = readInt("How many would you like to buy? ");
         if(buy <= STARTING_ITEMS_IN_INVENTORY)
         {
             STARTING_ITEMS_IN_INVENTORY = STARTING_ITEMS_IN_INVENTORY - buy;
-            println("Now we have " + STARTING_ITEMS_IN_INVENTORY + " left.");
+            console.log("Now we have " + STARTING_ITEMS_IN_INVENTORY + " left.");
+			console.log("");
         }
         
         if(buy > 20)
         {
-            println("There is not enough in inventory for that purchase.");
+            console.log("There is not enough in inventory for that purchase.");
+			console.log("");
         }
         
         if(STARTING_ITEMS_IN_INVENTORY == 0)
         {
-            println("All out!");
+            console.log("All out!");
         }
-        
     }
     
 }
+
+main();
